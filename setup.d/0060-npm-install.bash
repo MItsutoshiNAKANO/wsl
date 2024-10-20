44d34f5fa38d61dc650050fa2f0bb4a340a25414
Mitsutoshi Nakano <ItSANgo@gmail.com>
2024-10-21 06:40:46 +0900

    Install NPM packages

---
+++ setup.d/0060-npm-install.bash
@@ -0,0 +1,10 @@
+#! /bin/bash -ei
+
+## \file
+# \date 2024
+# \copyright ItSANgo@gmail.com
+# SPDX-License-Identifier: AGPL-3.0-or-later
+
+set -x
+npm install --location=global npm
+npm install --location=global --loglevel=verbose standard jsdoc
