974a46e108ecb6bec936cfd4b8216ae966a084ff
Mitsutoshi Nakano <ItSANgo@gmail.com>
2024-11-10 13:09:38 +0900

    Install Perl 5.40

---
+++ setup.d/0070-install-perl.bash
@@ -0,0 +1,9 @@
+#! /bin/bash -ei
+
+## \file
+# \date 2024
+# \copyright ItSANgo@gmail.com
+# SPDX-License-Identifier: AGPL-3.0-or-later
+
+perlbrew --notest install perl-5.40.0
+perlbrew switch perl-5.40.0
