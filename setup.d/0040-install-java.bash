22b22369ea7b47f3db541ec287798571f6c4d891
Mitsutoshi Nakano <ItSANgo@gmail.com>
2025-02-24 14:43:13 +0900

    Add Gradle

--- setup.d/0040-install-java.bash
+++ setup.d/0040-install-java.bash
@@ -1,8 +1,11 @@
 #! /bin/bash -ei
 
-## \file
-# \date 2024
-# \copyright ItSANgo@gmail.com
-# SPDX-License-Identifier: AGPL-3.0-or-later
+##
+# @file
+# @date 2025
+# @copyright
+#   ItSANgo@gmail.com
+#   SPDX-License-Identifier: AGPL-3.0-or-later
 
 sdk install java
+sdk install gradle
